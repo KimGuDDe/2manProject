2c91b5aebde1ec332384c399f9b1505e3e1a9629
1suckk <cwsgcd@gmail.com>
2025-03-09 21:58:06 +0900

    0309 1300 마이페이지 기초완성

--- src/main/java/data/controller/MemberController.java
+++ src/main/java/data/controller/MemberController.java
@@ -1,8 +1,43 @@
 package data.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import data.dto.LoginDataDto;
+import data.service.LoginDataService;
+import jakarta.servlet.http.HttpSession;
+import naver.storage.NcpObjectStorageService;
 
 @Controller
 public class MemberController {
+	@Autowired
+	LoginDataService loginDataService;
 	
+	@Autowired
+	NcpObjectStorageService storageService;
+	
+	//버켓 이름
+	private String bucketName = "bitcamp-bucket-103";
+	
+	@GetMapping("/member/mypage")
+	public String goMypage(HttpSession session, Model model)
+	{
+		//회원의 정보 가져오기
+		LoginDataDto user = (LoginDataDto)session.getAttribute("loggedInUser");
+		
+		if(user == null)
+		{
+			return "redirect:/";
+		}
+		else
+		{ 
+			model.addAttribute("user", user);
+			return "member/mypage";
+		}
+	
+	}
 }
