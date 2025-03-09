f4200585056f0737085fc95d86fbaf1f5c399a17
1suckk <cwsgcd@gmail.com>
2025-03-09 23:52:53 +0900

    0309 2200 채원석 마이페이지 기능 완성

--- src/main/java/data/controller/MemberController.java
+++ src/main/java/data/controller/MemberController.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import data.dto.ContectDto;
 import data.dto.LoginDataDto;
+import data.service.ContectService;
 import data.service.LoginDataService;
 import jakarta.servlet.http.HttpSession;
 import naver.storage.NcpObjectStorageService;
@@ -16,7 +18,8 @@ import naver.storage.NcpObjectStorageService;
 public class MemberController {
 	@Autowired
 	LoginDataService loginDataService;
-	
+	@Autowired
+	ContectService contectService;
 	@Autowired
 	NcpObjectStorageService storageService;
 	
@@ -29,13 +32,24 @@ public class MemberController {
 		//회원의 정보 가져오기
 		LoginDataDto user = (LoginDataDto)session.getAttribute("loggedInUser");
 		
+		List<ContectDto> list = null;
+		list=contectService.getSelectContect();
+		
 		if(user == null)
 		{
 			return "redirect:/";
 		}
 		else
 		{ 
+			for(int i=0; i<list.size(); i++)
+			{
+				ContectDto cdto = list.get(i);
+				System.out.println("문의내역: "+cdto);
+			}
+			
 			model.addAttribute("user", user);
+			model.addAttribute("list", list);
+			
 			return "member/mypage";
 		}
 	
